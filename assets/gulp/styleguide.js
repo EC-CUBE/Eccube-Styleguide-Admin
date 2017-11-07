2c496d3a51033e6667399c22dfd89a81772fe97d
t.goto <t.goto@chatbox-inc.com>
2017-11-07 18:59:06 +0900

    resolve #1

--- assets/gulp/styleguide.js
+++ assets/gulp/styleguide.js
@@ -20,7 +20,8 @@ const styelguideConfig = {
   disableEncapsulation: true,
   enablePug: true,
   extraHead:[
-    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">'
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">',
+    '<style>.sg-logo,.sg-design{display:none}</style>'
   ],
 }
 
